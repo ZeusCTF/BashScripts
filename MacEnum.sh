be648e5c34553d5a41659a1dd2f60d91f812cfc3
ZeusCTF <55359988+ZeusCTF@users.noreply.github.com>
2023-12-30 14:13:31 -0500

    v1

--- MacEnum.sh
+++ MacEnum.sh
@@ -5,18 +5,39 @@ echo "KERNEL/SYSTEM INFO"
 echo ""
 uname -a
 echo ""
+echo "Apple kernel extensions"
+ls /System/Library/Extensions
+echo ""
+echo "Custom kernel extensions"
+ls /Library/Extensions
+echo ""
 echo "Sudo version:"
 sudo -V | grep "Sudo ver"
 echo ""
-echo "Disk information:" #Native command for MACs, could work on Linux
+echo "Disk information:" #Native command for MACs
 diskutil list
+echo ""
+echo "Is MacOS GateKeeper enabled?"
+spctl --status
+echo ""
+echo "Installed Applications"
+ls /Applications
+echo ""
+echo "System Applications"
+ls /System/Applications
+echo ""
+system_profiler SPApplicationsDataType
+echo ""
+echo "More system info"
+system_profiler SPSoftwareDataType
+echo ""
 echo "----------"
 echo "NETWORK INFO"
 echo ""
 ip -4 addr
 # Check the exit status of the command
 if [ $? -ne 0 ]; then
-    echo "Error: The command failed, are you on a Mac?"
+    echo "Error: The command failed, trying ifconfig"
     #by default, ip does not appear to be installed on Mac systems, script will try to run ifconifg if ip fails
     ifconfig
 fi
@@ -25,15 +46,9 @@ echo "ARP Information:"
 arp -a
 echo ""
 scutil --dns
-# Check the exit status of the command
-if [ $? -ne 0 ]; then
-    echo "Error: The command failed, you are probably on linux"
-    #MacOS does not use /etc/resolv.conf, first checking if the above command works
-    #Then if it doesnt, then we can look for the hostname 
-    cat /etc/resolv.conf | grep hostname
-fi
 echo "----------"
 echo ""
+
 #basic user enum
 echo "USER INFO"
 echo "----------------"
@@ -42,6 +57,11 @@ id_output=$(id)
 for line in $id_output; do
     echo "$line"
 done
+echo "Trying to read messages:"
+sqlite3 $HOME/Library/Messages/chat.db 'select * from message'
+echo ""
+echo "Files that reference a 'password'"
+mdfind password
 echo ""
 echo "Login information:"
 last
@@ -53,13 +73,8 @@ env_output=$(env)
 for line in $env_output; do
     echo $line
 done
-
-
 echo ""
 echo ""
-
-
-
 echo "Do you have access to write to a PATH folder?"
 IFS=:
 
@@ -97,5 +112,7 @@ echo ""
 echo "Current running processes:"
 echo ""
 ps aux
+
+
 
 
